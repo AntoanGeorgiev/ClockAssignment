d272253eaa4664561901e04f494f2dd8a4c6ae93
Antoan Georgiev <antoan@blabla.com>
2023-04-25 23:32:45 -0400

    Fixes

--- Assets/Scripts/ClockManager.cs
+++ Assets/Scripts/ClockManager.cs
@@ -2,37 +2,44 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ClockManager : MonoBehaviour
 {
     [SerializeField] Transform hours;
     [SerializeField] Transform minutes;
     [SerializeField] Transform seconds;
-    float hoursRotation = -30f, minutesRotation = -6f, secondsRotation = -6f;
+    float hoursRotation = -30f, minutesRotation = -6f, secondsRotation = -6f; // 60 minutes and seconds so starting at -6f, 12 hours so starting -30f;
     bool a = true;
+    public UnityEvent MonitorHM;
     void Awake()
     {
-        DateTime time = DateTime.Now;
+        DateTime time = DateTime.Now; // Retrieve the time on Awake
         hours.localRotation = Quaternion.Euler(0f, 0f, hoursRotation * time.Hour);
         minutes.localRotation = Quaternion.Euler(0f, 0f, minutesRotation * time.Minute);
         seconds.localRotation = Quaternion.Euler(0f, 0f, secondsRotation * time.Second);
     }
     void Update()
     {
-        var time = DateTime.Now;
+        var time = DateTime.Now; // Keep updating the time every frame
         hours.localRotation = Quaternion.Euler(0f, 0f, hoursRotation * time.Hour);
         minutes.localRotation = Quaternion.Euler(0f, 0f, minutesRotation * time.Minute);
         seconds.localRotation = Quaternion.Euler(0f, 0f, secondsRotation * time.Second);
-        if (minutes.rotation == hours.rotation && a)
+        if (minutes.rotation == hours.rotation && a) //If minute and hour are on top of each other
         {
-            Debug.Log("Hours and minutes align!");
-            a = false;
-            StartCoroutine(TrackHoursMinutesAgain());
+            MonitorHM.Invoke();         
         }
     }
     IEnumerator TrackHoursMinutesAgain()
     {
-        yield return new WaitForSeconds(61f);
+        yield return new WaitForSeconds(60f); // We wait for a minute to avoid excessive event usage
         a = true;
+    }
+
+    public void WhenHMAlign() // Debug that the values align and reset the value of a;
+    {
+        Debug.Log("Hours and minutes align!");
+        a = false;
+        StartCoroutine(TrackHoursMinutesAgain());
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -12,34 +12,34 @@ public class Spawner : MonoBehaviour
     [SerializeField] Material secondsMaterial;
     void Start()
     {
-        float b = UnityEngine.Random.Range(1f, 4f);
-        if (numberOfObjects > 10) { numberOfObjects = 10; }
-        for (int i = 0; i < numberOfObjects; i++)
+        float b = UnityEngine.Random.Range(1f, 4f); // Bool for exercise 4
+        if (numberOfObjects > 10) { numberOfObjects = 10; } //Set a limit on maximum number of clothes that can fit in a scene
+        for (int i = 0; i < numberOfObjects; i++) // There is already one clock in the scene from exercise 1 so the real number of clocks is this +1
         {
             Vector3 randomPosition = new Vector3 (Random.Range(-9f,9f), Random.Range(-4f, 4f), 0f);
             Collider[] colliders = Physics.OverlapSphere(randomPosition, 1.5f);
-            if (colliders.Length == 0)
+            if (colliders.Length == 0) // By checking that the objects don't collide we make sure they don't superpose
             {
                 Instantiate(prefab, randomPosition, Quaternion.identity);
             }
-            else
+            else // We keep trying till we get a working scenario
             {
                 i--;
             }
         }
-        if (b >= 1f && b < 2f)
+        if (b >= 1f && b < 2f)  // Exercise 4 case
         {
             hourMaterial.color = Color.blue;
             minutesMaterial.color = Color.yellow;
             secondsMaterial.color = Color.red;
         }
-        if (b >= 2f && b < 3f)
+        if (b >= 2f && b < 3f) // Exercise 4 case
         {
             hourMaterial.color = Color.cyan;
             minutesMaterial.color = Color.grey;
             secondsMaterial.color = Color.gray;
         }
-        if (b >= 3f && b < 4f)
+        if (b >= 3f && b < 4f) // Exercise 4 case
         {
             hourMaterial.color = Color.black;
             minutesMaterial.color = Color.black;
@@ -49,13 +49,13 @@ public class Spawner : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)) // If we get space clicked, we trigger an input event
         {
             InputEvent.Invoke();
         }
     }
 
-    public void AddAnotherClock()
+    public void AddAnotherClock() // The input event when clicking space
     {
         if (numberOfObjects < 10)
         {
@@ -70,9 +70,9 @@ public class Spawner : MonoBehaviour
                     a = true;
                 }
             }
-            while (!a);
+            while (!a); // makes sure that we won't get empty space, if there is no validation at some cases there won't be a new clock because the pos will superpose
             a = false;
         }
-        else Debug.Log("Too many clocks!");
+        else Debug.Log("Too many clocks!"); //making sure the clocks aren't over the limit
     }
  }
