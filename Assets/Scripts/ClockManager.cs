bba2c87878bd42d828039a98ba211a230c712087
Antoan Georgiev <antoan@blabla.com>
2023-04-25 23:16:36 -0400

    Exercise6

--- Assets/Scripts/ClockManager.cs
+++ Assets/Scripts/ClockManager.cs
@@ -9,6 +9,7 @@ public class ClockManager : MonoBehaviour
     [SerializeField] Transform minutes;
     [SerializeField] Transform seconds;
     float hoursRotation = -30f, minutesRotation = -6f, secondsRotation = -6f;
+    bool a = true;
     void Awake()
     {
         DateTime time = DateTime.Now;
@@ -22,5 +23,16 @@ public class ClockManager : MonoBehaviour
         hours.localRotation = Quaternion.Euler(0f, 0f, hoursRotation * time.Hour);
         minutes.localRotation = Quaternion.Euler(0f, 0f, minutesRotation * time.Minute);
         seconds.localRotation = Quaternion.Euler(0f, 0f, secondsRotation * time.Second);
+        if (minutes.rotation == hours.rotation && a)
+        {
+            Debug.Log("Hours and minutes align!");
+            a = false;
+            StartCoroutine(TrackHoursMinutesAgain());
+        }
+    }
+    IEnumerator TrackHoursMinutesAgain()
+    {
+        yield return new WaitForSeconds(61f);
+        a = true;
     }
 }
